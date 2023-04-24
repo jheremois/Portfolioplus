6f29acac9df8091447ad5828ff0b5219392821b1
jheremy <jheremy802@gmail.com>
2023-04-23 21:48:31 -0400

    add profile contact form

--- pages/jheremy.tsx
+++ pages/jheremy.tsx
@@ -3,6 +3,233 @@ import Image from 'next/image';
 import {LinkIcon} from "@heroicons/react/24/outline"
 import Project from '../components/Project';
 import Link from 'next/link';
+import { useRef, useState } from 'react';
+import * as Yup from "yup";
+
+interface contactForm{
+    name: string
+    email: string
+    company: string
+    number: string
+    message: string
+}
+
+const FormModal = ()=>{
+    const [formData, setFormData] = useState<contactForm>({
+        name: "",
+        company: "",
+        email: "",
+        number: "",
+        message: "",
+    });
+
+    const [errors, setErrors] = useState<contactForm>({
+        name: "",
+        email: "",
+        company: "",
+        number: "",
+        message: "",
+    });
+
+    const schema = Yup.object().shape({
+        name: Yup.string().required("Name is required"),
+        company: Yup.string().required("Company is required"),
+        email: Yup.string()
+          .email("Email is invalid")
+          .required("Email is required"),
+        number: Yup.string(),
+        message: Yup.string().required("Message is required"),
+    });
+
+    const handleInputChange = async (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+    
+        try {
+    
+          await schema.validate(formData, { abortEarly: false });
+    
+          // Submit form data to server or perform further actions
+          console.log("Form data:", formData);
+          // Reset form data
+          setFormData({
+            name: "",
+            company: "",
+            email: "",
+            number: "",
+            message: "",
+          });
+          setErrors({ name: "", email: "", company: "", number: "", message: "" });
+        } catch (err) {
+          if (err instanceof Yup.ValidationError) {
+            const validationErrors: any = {
+                 name: "", email: "", company: "", number: "", message: ""
+            };
+            err.inner.forEach((error) => {
+                if (error.path !== undefined) {
+                    validationErrors[error.path] = error.message;
+                }
+            });
+            setErrors(validationErrors);
+          }
+        }
+    };
+
+    return(
+        <div className="w-screen bg-grey bg-opacity-30 fixed modal top-0 z-50 min-h-screen ">
+            <div 
+                className="
+                    py-4
+                    overflow-auto
+                    max-h-screen
+                    flex justify-center 
+                "
+            >
+                <form
+                    onSubmit={handleSubmit}
+                    className="
+                        w-11/12  md:w-2/5 2xl:w-1/3 bg-darkGray mt-4 md:mt-20 
+                        rounded-lg shadow-sm shadow-gray-600
+                        p-9 flex-col flex gap-6 h-fit
+                    "
+                >
+                    <div className="">
+                        <h4 className='text-xl font-semibold'>
+                            Send a message to Jheremy
+                        </h4>
+                        <p className='text-lg font-normal text-bodyText'>
+                            Send a message to Jheremy
+                        </p>
+                    </div>
+                    <div className="">
+                        <div className="flex gap-3 flex-col md:flex-row justify-between">
+                            <div className="mb-4 w-full">
+                                <label htmlFor="name" className="block mb-1 text-sm">
+                                    Full Name:
+                                </label>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    name="name"
+                                    value={formData.name}
+                                    onChange={handleInputChange}
+                                    className="
+                                        bg-grey
+                                        w-full px-4 py-2 border border-gray-400 
+                                        rounded focus:outline-none focus:border-gray-200
+                                    "
+                                />
+                                {errors.name && (
+                                <p className="text-red-500 text-xs italic">{errors.name}</p>
+                                )}
+                            </div>
+                            <div className="mb-4 w-full">
+                                <label htmlFor="company" className="block mb-1 text-sm">
+                                    Company Name / Charge:
+                                </label>
+                                <input
+                                    type="text"
+                                    id="company"
+                                    name="company"
+                                    value={formData.company}
+                                    onChange={handleInputChange}
+                                    className="
+                                        bg-grey
+                                        w-full px-4 py-2 border border-gray-400 
+                                        rounded focus:outline-none focus:border-gray-200
+                                    "
+                                />
+                                {errors.company && (
+                                <p className="text-red-500 text-xs italic">{errors.company}</p>
+                                )}
+                            </div>
+                        </div>
+                        <div className="flex gap-3 flex-col md:flex-row justify-between">
+                            <div className="mb-4 w-full">
+                                <label htmlFor="mail" className="block mb-1 text-sm">
+                                    E-mail:
+                                </label>
+                                <input
+                                    type="text"
+                                    id="email"
+                                    name="email"
+                                    value={formData.email}
+                                    onChange={handleInputChange}
+                                    className="
+                                        bg-grey
+                                        w-full px-4 py-2 border border-gray-400 
+                                        rounded focus:outline-none focus:border-gray-200
+                                    "
+                                />
+                                {errors.email && (
+                                <p className="text-red-500 text-xs italic">{errors.email}</p>
+                                )}
+                            </div>
+                            <div className="mb-4 w-full">
+                                <label htmlFor="phone" className="block mb-1 text-sm">
+                                    Phone (optional):
+                                </label>
+                                <input
+                                    type="number"
+                                    id="number"
+                                    name="number"
+                                    value={formData.number}
+                                    onChange={handleInputChange}
+                                    className="
+                                        bg-grey
+                                        w-full px-4 py-2 border border-gray-400 
+                                        rounded focus:outline-none focus:border-gray-200
+                                    "
+                                />
+                                {errors.number && (
+                                <p className="text-red-500 text-xs italic">{errors.number}</p>
+                                )}
+                            </div>
+                        </div>
+                        <div className="">
+                            <div className="mb-4">
+                                <label htmlFor="name" className="block mb-1 text-sm">
+                                    Message:
+                                </label>
+                                <textarea
+                                    rows={2}
+                                    id="message"
+                                    name="message"
+                                    value={formData.message}
+                                    onChange={handleInputChange}
+                                    className="
+                                        resize-none
+                                        bg-grey
+                                        w-full px-4 py-2 border border-gray-400 
+                                        rounded focus:outline-none focus:border-gray-200
+                                    "
+                                />
+                                {errors.message && (
+                                <p className="text-red-500 text-xs italic">{errors.message}</p>
+                                )}
+                            </div>
+                        </div>
+                        <button 
+                            className="
+                            mt-3
+                            rounded-xl py-2 px-8 bg-primary
+                            hover:bg-primaryAlt w-full duration-100
+                            "
+                        >
+                            Lets Talk
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
 
 const Jheremy: NextPage = (props: any) => {
 
@@ -76,6 +303,7 @@ const Jheremy: NextPage = (props: any) => {
   
   return (
     <>
+        <FormModal/>
         <div className="max-w-7xl mx-auto">
             <div className="p-7 md:p-12">
                 <div 
